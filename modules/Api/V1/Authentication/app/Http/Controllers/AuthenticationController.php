3f669dbf9b1e98615c28b7a462aa67bf99e8b994
aminsarshar <sarsharamin85@gmail.com>
2024-10-08 18:55:34 +0330

    send sms for authentication

--- modules/Api/V1/Authentication/app/Http/Controllers/AuthenticationController.php
+++ modules/Api/V1/Authentication/app/Http/Controllers/AuthenticationController.php
@@ -3,6 +3,7 @@
 namespace Modules\Api\V1\Authentication\app\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\SmsCode;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
@@ -13,7 +14,7 @@ class AuthenticationController extends Controller
 {
     public function register(Request $request)
     {
-        $validator = Validator::make($request->all() , [
+        $validator = Validator::make($request->all(), [
             'firstname' => 'required|string',
             'lastname' => 'required|string',
             'username' => 'required|string|unique:users,username',
@@ -22,7 +23,7 @@ class AuthenticationController extends Controller
             'password_confirmation' => 'required|same:password',
         ]);
 
-        if($validator->fails()){
+        if ($validator->fails()) {
             return errorResponse(422, $validator->messages());
         }
 
@@ -32,40 +33,40 @@ class AuthenticationController extends Controller
             'username' => $request->username,
             'email' => $request->email,
             'password' => Hash::make($request->password),
-            'provider_name' => 'manual'
+            'provider_name' => 'manual',
         ]);
 
         $token = $user->createToken('myApp')->plainTextToken;
 
         return response()->json([
             'user' => $user,
-            'token' => $token
-        ] , 201);
+            'token' => $token,
+        ], 201);
 
         return successResponse([
             'data' => new AuthenticationResource($user),
-            'token' => $token
+            'token' => $token,
         ], 200, 'حساب کاربری با موفقیت ایجاد شد.');
     }
 
     public function login(Request $request)
     {
-        $validator = Validator::make($request->all() , [
+        $validator = Validator::make($request->all(), [
             'email' => 'required|email',
-            'password' => 'required|string'
+            'password' => 'required|string',
         ]);
 
-        if($validator->fails()){
-            return response()->json($validator->messages() , 422);
+        if ($validator->fails()) {
+            return response()->json($validator->messages(), 422);
         }
 
-        $user = User::where('email' , $request->email)->first();
+        $user = User::where('email', $request->email)->first();
 
-        if(!$user){
+        if (!$user) {
             return errorResponse(401, 'کاربر یافت نشد!');
         }
 
-        if(!Hash::check($request->password , $user->password)){
+        if (!Hash::check($request->password, $user->password)) {
             return errorResponse(401, 'رمز عبور اشتباه است!');
         }
 
@@ -73,9 +74,48 @@ class AuthenticationController extends Controller
 
         return successResponse([
             'data' => new AuthenticationResource($user),
-            'token' => $token
+            'token' => $token,
         ], 200, 'کاربر با موفقیت وارد شد.');
 
+    }
+
+    public function sendSms(Request $request)
+    {
+        $phone_number = $request->input('phone_number');
+        $checkLastSms = SmsCode::checkTwoMinute($phone_number);
+        if ($checkLastSms == null) {
+            $code = rand(111111, 999999);
+            SmsCode::query()->create([
+                'phone_number' => $phone_number,
+                'code' => $code,
+            ]);
+
+            return response()->json([
+                'resault' => true,
+                'message' => "اس ام اس ارسال شد",
+                'data' => [
+                    'phone_number' => $phone_number,
+                    'code' => $code,
+
+                ],
+            ], 201);
+
+            return successResponse([
+                'data' => [
+                    'phone_number' => $phone_number,
+                    'code' => $code,
+
+                ],
+            ], 200, 'حساب کاربری با موفقیت ایجاد شد.');
+
+        } else {
+            return response()->json([
+                'resault' => false,
+                'message' => "اس ام اس ارسال نشد",
+                'data' => [],
+            ], 403);
+
+        }
     }
 
     // public function logout()
--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ Route::get('/user', function (Request $request) {
 
 Route::prefix('v1/')->name('v1.')->group(function () {
     Route::post('register' , [AuthenticationController::class , 'register'])->name('register');
+    Route::post('send_sms' , [AuthenticationController::class , 'sendSms'])->name('send_sms');
     Route::post('login' , [AuthenticationController::class , 'login']);
     Route::post('logout' , [AuthenticationController::class , 'logout'])->middleware('auth:sanctum');
 });
